e6c458b4300fd61951986bdc5f914ad9972483b8
eeappiah <66851526+eeappiah@users.noreply.github.com>
2021-10-20 23:00:25 +0100

    New changes

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import socket
 from _thread import *
+from player import Player
+import pickle
+import sys
 
 server = "192.168.1.74"
 port = 5555
@@ -15,38 +18,28 @@ s.listen(2)
 print("Waiting for connection, Server Started")
 
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
+players = [Player(0,0,50,50,(255,0,0)), Player(0,0,50,50,(0,255,0))]
 
 
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-
-pos = [(0, 0), (100, 100)]
-
-
-def thread_client(connect, currentPlayer):
-    conn.send(str.encode(make_pos(pos[currentPlayer])))
+def thread_client(conn, player):
+    conn.send(pickle.dumps(players[player]))
     reply = ""
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[currentPlayer] = data
-
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
-                if currentPlayer == 1:
-                    reply = pos[0]
+                if player == 1:
+                    reply = players[0]
                 else:
-                    reply = pos[1]
+                    reply = players[1]
                 print("Received: ", reply)
                 print("Sending: ", reply)
-            connect.sendall(str.encode(make_pos(reply)))
+            conn.sendall(pickle.dumps(reply))
         except:
             break
 
