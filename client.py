65bfe8376111e276c33dc5ab1cafa7d5bb594ca3
eeappiah <66851526+eeappiah@users.noreply.github.com>
2021-10-20 20:31:15 +0100

    New

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import pygame
 
+from network import Network
+
 width = 500
 height = 500
 window = pygame.display.set_mode((width, height))
@@ -32,27 +34,46 @@ class Player():
             self.y -= self.vel
         if keys[pygame.K_DOWN]:
             self.y += self.vel
-
+        self.update()
+    def update(self):
         self.rect = (self.x, self.y, self.width, self.height)
 
 
-def redrawWindow(window, player):
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+
+def redrawWindow(window, player, player2):
     window.fill((255, 255, 255))
     player.draw(window)
+    player2.draw(window)
     pygame.display.update()
 
 
 def main():
     run = True
-    p = Player(50, 50, 100, 100, (0, 255, 0))
+    n = Network()
+    startPos = read_pos(n.getPos())
+    p = Player(startPos[0], startPos[1], 100, 100, (0, 255, 0))
+    p2 = Player(0, 0, 100, 100, (0, 255, 0))
     clock = pygame.time.Clock()
     while run:
+        clock.tick(60)
+        p2pos = read_pos(n.send(make_pos((p.x, p.y))))
+        p2.x = p2pos[0]
+        p2.y = p2pos[1]
+        p2.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
         p.move()
-        redrawWindow(window, p)
+        redrawWindow(window, p, p2)
 
 
 main()
--- server.py
+++ server.py
@@ -15,21 +15,38 @@ s.listen(2)
 print("Waiting for connection, Server Started")
 
 
-def thread_client(connect):
-    conn.send(str.encode("Connected"))
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+
+pos = [(0, 0), (100, 100)]
+
+
+def thread_client(connect, currentPlayer):
+    conn.send(str.encode(make_pos(pos[currentPlayer])))
     reply = ""
     while True:
         try:
-            data = connect.recv(2048)
-            reply = data.decode("utf-8")
+            data = read_pos(conn.recv(2048).decode())
+            pos[currentPlayer] = data
+
 
             if not data:
                 print("Disconnected")
                 break
             else:
+                if currentPlayer == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
                 print("Received: ", reply)
                 print("Sending: ", reply)
-            connect.sendall(str.encode(reply))
+            connect.sendall(str.encode(make_pos(reply)))
         except:
             break
 
@@ -37,9 +54,11 @@ def thread_client(connect):
     conn.close()
 
 
+currentPlayer = 0
 
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
 
-    start_new_thread(thread_client, (conn,))
+    start_new_thread(thread_client, (conn, currentPlayer))
+    currentPlayer += 1
